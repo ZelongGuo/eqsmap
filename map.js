307e75f69bbe57bece6a44dbc80c9aab8ff5c829
zelong <zelong.guo@outlook.com>
2025-07-09 15:10:09 +0800

    update to tianditu

--- map.js
+++ map.js
@@ -6,19 +6,19 @@ var map = L.map('map').setView([35.0, 105.0], 5); // set initial center and scal
 // }).addTo(map);
 
 
-L.tileLayer('https://webrd0{s}.is.autonavi.com/appmaptile?style=7&x={x}&y={y}&z={z}', {
-    subdomains: '1234',
-    maxZoom: 18,
-    attribution: 'Map data &copy; 高德地图'
-}).addTo(map);
+// L.tileLayer('https://webrd0{s}.is.autonavi.com/appmaptile?style=7&x={x}&y={y}&z={z}', {
+//     subdomains: '1234',
+//     maxZoom: 18,
+//     attribution: 'Map data &copy; 高德地图'
+// }).addTo(map);
 
 // <script src="http://api.tianditu.gov.cn/api?v=4.0&tk=您的密钥" type="text/javascript"></script>
 
 
-// L.tileLayer('https://t{s}.tianditu.gov.cn/DataServer?T=vec_w&x={x}&y={y}&l={z}&tk=你的Key', {
-//     subdomains: ['0','1','2','3','4','5','6','7'],
-//     maxZoom: 18,
-//     attribution: '天地图'
-// }).addTo(map);
+L.tileLayer('https://t{s}.tianditu.gov.cn/DataServer?T=vec_w&x={x}&y={y}&l={z}&tk=072939ab9bd921a6fe546aea63310050', {
+    subdomains: ['0','1','2','3','4','5','6','7'],
+    maxZoom: 18,
+    attribution: '天地图'
+}).addTo(map);
 
 
