5884007cbbe2cfb672156a6da001de9d7842f5e6
zelong <zelong.guo@outlook.com>
2025-07-09 13:48:59 +0800

    update

--- map.js
+++ map.js
@@ -1,4 +1,4 @@
-var map = L.map('eqsmap').setView([35.0, 105.0], 5); // set initial center and scaling
+var map = L.map('map').setView([35.0, 105.0], 5); // set initial center and scaling
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
